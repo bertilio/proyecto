8417ddb2d3dfe82aefca07d5da5baecad41240f0
bertilio <alberto.vicentee@edu.uah.es>
2022-01-16 22:43:16 +0100

    Todo funsiona

--- ArchivosPracticaFinal_v4/Matlab/Simulacion.m
+++ ArchivosPracticaFinal_v4/Matlab/Simulacion.m
@@ -1,13 +1,8 @@
 %% Controlador 1
 sim('Controlador1.slx');
 
-angulo = phi.signals.values(1,:)
 
-angulo = angulo.';
-
-borrar = sum(isnan(angulo)) + 1;
-
-training = [sonar1.signals.values(borrar:end),sonar2.signals.values(borrar:end),sonar3.signals.values(borrar:end),sonar4.signals.values(borrar:end),x.signals.values(borrar:end),y.signals.values(borrar:end),angulo(borrar:end),w.signals.values(borrar:end),v.signals.values(borrar:end)];
+training = [sonar1.signals.values(borrar:end),sonar2.signals.values(borrar:end),sonar3.signals.values(borrar:end),sonar4.signals.values(borrar:end),w.signals.values(borrar:end),v.signals.values(borrar:end)];
 
 
 save datos_entrenamiento training
